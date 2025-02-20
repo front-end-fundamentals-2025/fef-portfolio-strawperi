6916972d82fc55938516de5384de85d6d70c508b
Peri Muhtaroglu <p.muhtaroglu@gmail.com>
2025-02-20 12:30:44 +0100

    event listener

---
+++ js/main.js
@@ -0,0 +1,3 @@
+const homeButton = document.getElementById("home-button");
+const worksButton = document.getElementById("works-button");
+const contactButton = document.getElementById("conact-button");
