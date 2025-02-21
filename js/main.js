853df882d24924f1567f889b8d9d876c377e0b8b
Peri Muhtaroglu <p.muhtaroglu@gmail.com>
2025-02-21 11:21:36 +0100

    omg it works now

--- js/main.js
+++ js/main.js
@@ -1,34 +1,19 @@
-// const logo = document.getElementById("logo");
+const homeLink = document.getElementById("home-link");
+const worksLink = document.getElementById("works-link");
+const contactLink = document.getElementById("contact-link");
 
-// logo.addEventListener("mouseover", function () {
-//   logo.innerText.color = "red";
-// });
+// Function to add hover effect with a custom color
+function addHoverEffect(element, hoverColor, originalColor) {
+  element.addEventListener("mouseover", function () {
+    this.style.color = hoverColor;
+  });
 
-// logo.addEventListener("mouseout", function () {
-//   logo.innerText.color = "black";
-// });
-// const homeLink = document.getElementById("home-link");
-// const worksLink = document.getElementById("works-link");
-// const contactLink = document.getElementById("contact-link");
+  element.addEventListener("mouseout", function () {
+    this.style.color = originalColor;
+  });
+}
 
-// // Function to add hover effect with a custom color
-// function addHoverEffect(element, hoverColor, originalColor) {
-//   element.addEventListener("mouseover", function () {
-//     this.style.color = hoverColor;
-//   });
-
-//   element.addEventListener("mouseout", function () {
-//     this.style.color = originalColor;
-//   });
-// }
-
-// // Apply hover effects to each link with different colors
-// addHoverEffect(homeLink, "red", "black");
-// addHoverEffect(worksLink, "blue", "black");
-// addHoverEffect(contactLink, "green", "black");
-const redButton = document.getElementById("red-button");
-const bodyElement = document.querySelector("body");
-
-redButton.addEventListener("click", function (event) {
-  bodyElement.style.backgroundColor = "ff0000";
-});
+// Apply hover effects to each link with different colors
+addHoverEffect(homeLink, "red", "black");
+addHoverEffect(worksLink, "blue", "black");
+addHoverEffect(contactLink, "green", "black");
